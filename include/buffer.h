9dc57f019f893872f579533473a7e9ccbbbcb2f9
César Augusto <c.augusto0302@gmail.com>
2025-03-26 19:57:44 -0300

    Adjusting the Buffer class conform to the group-defined pattern

--- include/buffer.h
+++ include/buffer.h
@@ -11,37 +11,69 @@ class Buffer{
         static constexpr size_t MAX_SIZE = Ethernet::MTU;
 
     public:
-        Buffer() : _size(0) {
-            memset(_data, 0, MAX_SIZE);
-        };
+        Buffer();
 
-        Buffer(const void* data, size_t size) {
-            setData(data, size);
-        };
+        Buffer(const void* data, std::size_t size);
 
-        ~Buffer() = default;
+        ~Buffer();
 
-        const T* data() const { return reinterpret_cast<T*>(_data); };
+        const T* data() const;
 
-        void setData(const void* data, size_t size) {
-            setSize(size);
-            memcpy(_data, data, _size);
-        };
+        void setData(const void* data, size_t size);
 
-        const size_t size() const { return _size; };
+        const std::size_t size() const;
 
-        void setSize(size_t size) {
-            _size = (size > MAX_SIZE) ? MAX_SIZE : size; // Ensures that it does not exceed MTU
-        };
+        void setSize(std::size_t size);
 
-        void clear() {
-            memset(_data, 0, MAX_SIZE);
-            _size = 0;
-        };
+        void clear();
 
     private:
-        uint8_t _data[MAX_SIZE];
-        size_t _size;
+        std::uint8_t _data[MAX_SIZE];
+        std::size_t _size;
 };
+
+
+/********* Buffer Implementation **********/
+template <typename T>
+Buffer<T>::Buffer() : _size(0) {
+    std::memset(_data, 0, MAX_SIZE);
+}
+
+template <typename T>
+Buffer<T>::Buffer(const void* data, std::size_t size) {
+    setData(data, size);
+}
+
+template <typename T>
+Buffer<T>::~Buffer() {
+    clear();
+}
+
+template <typename T>
+const T* Buffer<T>::data() const {
+    return reinterpret_cast<T*>(_data);
+}
+
+template <typename T>
+void Buffer<T>::setData(const void* data, std::size_t size) {
+    setSize(size);
+    std::memcpy(_data, data, _size);
+}
+
+template <typename T>
+const std::size_t Buffer<T>::size() const {
+    return _size;
+}
+
+template <typename T>
+void Buffer<T>::setSize(std::size_t size) {
+    _size = (size > MAX_SIZE) ? MAX_SIZE : size; // Ensures that it does not exceed MTU
+}
+
+template <typename T>
+void Buffer<T>::clear() {
+    std::memset(_data, 0, MAX_SIZE);
+    _size = 0;
+}
 
 #endif // BUFFER_H
