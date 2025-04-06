c9e458116d63d6055ffdd9be223e9fb19cbde92b
César Augusto <c.augusto0302@gmail.com>
2025-04-06 16:39:06 -0300

    Inheritance had to be implemented in order for the code to compile correctly

--- include/observer.h
+++ include/observer.h
@@ -1,74 +1,88 @@
 #ifndef OBSERVER_H
 #define OBSERVER_H
 
+#include <unistd.h>  // for getpid()
 #include "semaphore_wrapper.h"
 #include "list.h"
-#include <atomic>
-#include <stdexcept>
-#include <unistd.h>  // for getpid()
-#include <fcntl.h>   // for O_CREAT, O_EXCL
 
-// Forward declarations for observed classes
+// Forward declarations for Conditionally_Data_Observed class
 template <typename T, typename Condition>
 class Conditionally_Data_Observed;
-
-template <typename D, typename C>
-class Concurrent_Observed;
 
 // Fundamentals for Observer X Observed
 template <typename T, typename Condition>
 class Conditional_Data_Observer {
     friend class Conditionally_Data_Observed<T, Condition>;
-public:
-    typedef T Observed_Data;
-    typedef Condition Observing_Condition;
+    
+    public:
+        typedef T Observed_Data;
+        typedef Condition Observing_Condition;
+        typedef Conditionally_Data_Observed<T, Condition> Observed;
 
-    Conditional_Data_Observer() = default;
-    virtual ~Conditional_Data_Observer() = default;
+        Conditional_Data_Observer(Condition c);
+        ~Conditional_Data_Observer() = default;
 
-    virtual void update(Condition c, T* d) = 0;
-    virtual Condition rank();
-protected:
-    Condition _rank;
+        void update(Condition c, Observed_Data* d);
+        T* updated();
+
+        const Condition rank() { return _rank; };
+    
+    protected:
+        Condition _rank;
+        List<T> _data;
 };
+
+/*********************** CONDITIONAL_DATA_OBSERVER IMPLEMENTATION ************************/
+template <typename T, typename Condition>
+Conditional_Data_Observer<T, Condition>::Conditional_Data_Observer(Condition c) {
+    _rank = c;
+}
+
+template <typename T, typename Condition>
+void Conditional_Data_Observer<T, Condition>::update(Condition c, Observed_Data* d) {
+    if (c == _rank) {
+        _data.insert(d);
+    }
+}
+
+template <typename T, typename Condition>
+T* Conditional_Data_Observer<T, Condition>::updated() {
+    return _data.remove();
+}
+/*****************************************************************************************/
+
+
+// Foward declaration for Concurrent_Observed class;
+template <typename D, typename C>
+class Concurrent_Observed;
 
 // Conditional Observer x Conditionally Observed with Data decoupled by a Semaphore
 template<typename D, typename C>
-class Concurrent_Observer {
+class Concurrent_Observer : public Conditional_Data_Observer<D, C> {
     friend class Concurrent_Observed<D, C>;
-public:
-    typedef D Observed_Data;
-    typedef C Observing_Condition;
-public:
-    Concurrent_Observer(C rank);
-    virtual ~Concurrent_Observer() = default;
+
+    public:
+        typedef D Observed_Data;
+        typedef C Observing_Condition;
+        typedef Concurrent_Observed<D, C> Observed;
     
-    void update(C c, D* d);
-    D* updated();
-    C rank();
-    
-private:
-    SemaphoreWrapper _semaphore;
-    List<D> _data;
-    C _rank;
+    public:
+        Concurrent_Observer(C rank) : Conditional_Data_Observer<D, C>(rank) {};
+        ~Concurrent_Observer() = default;
+        
+        void update(C c, D* d);
+        D* updated();
+        
+    private:
+        SemaphoreWrapper _semaphore;
 };
 
-// Template implementations for Conditional_Data_Observer
-template <typename T, typename Condition>
-Condition Conditional_Data_Observer<T, Condition>::rank() {
-    return _rank;
-}
-
-// Template implementations for Concurrent_Observer
-template <typename D, typename C>
-Concurrent_Observer<D, C>::Concurrent_Observer(C rank)
-    : _semaphore(), _rank(rank) {}
-
+/***************** CONCURRENT_OBSERVER IMPLEMENTATION *************************/
 template <typename D, typename C>
 void Concurrent_Observer<D, C>::update(C c, D* d) {
-    if (c == _rank && d != nullptr) {
+    if (c == this->_rank && d != nullptr) {
         // Store a copy of the pointer, don't modify ref_count here
-        _data.insert(d);
+        this->_data.insert(d);
         _semaphore.post();
     }
 }
@@ -76,12 +90,8 @@ void Concurrent_Observer<D, C>::update(C c, D* d) {
 template <typename D, typename C>
 D* Concurrent_Observer<D, C>::updated() {
     _semaphore.wait();
-    return _data.remove();
+    return this->_data.remove();
 }
-
-template <typename D, typename C>
-C Concurrent_Observer<D, C>::rank() {
-    return _rank;
-}
+/*******************************************************************************/
 
 #endif // OBSERVER_H
