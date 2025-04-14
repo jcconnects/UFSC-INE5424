f0ce67459a21533cf4248ee6c039c5269a1a1375
jresmer <jpresmer@gmail.com>
2025-04-13 21:22:18 -0300

    address filtering added to NIC::handleSignal to avoid receiving a message from itself

--- include/nic.h
+++ include/nic.h
@@ -281,6 +281,8 @@ void NIC<Engine>::handleSignal() {
     
     // Convert protocol from network to host byte order
     frame.prot = ntohs(frame.prot);
+    // Filters out messages from itself
+    if (_address == frame.src) return;
     db<SocketEngine>(INF) << "[SocketEngine] received frame: {src = " << Ethernet::mac_to_string(frame.src) << ", dst = " << Ethernet::mac_to_string(frame.dst) << ", prot = " << frame.prot << ", size = " << bytes_received << "}\n";
     
     // Process the frame if callback is set
