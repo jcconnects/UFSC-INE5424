c8ca5f37b68eb45ced53471bfebaea1ca600ebc7
Joao Cordeiro <jschmidtcordeiro@gmail.com>
2025-05-30 14:59:25 -0300

    feat: add multiple logs to debug race condition

--- include/api/network/nic.h
+++ include/api/network/nic.h
@@ -105,19 +105,28 @@ class NIC: public Ethernet, public Conditionally_Data_Observed<Buffer<Ethernet::
 /*********** NIC Implementation ************/
 template <typename Engine>
 NIC<Engine>::NIC() : _running(true) {
-    // Engine starts on its own
-
+    // Initialize buffers FIRST - before starting Engine
+    db<NIC>(INF) << "[NIC] [constructor] initializing buffers and semaphores\n";
+    
     for (unsigned int i = 0; i < N_BUFFERS; ++i) {
         _buffer[i] = DataBuffer();
         _free_buffers.push(&_buffer[i]);
     }
-    db<NIC>(INF) << "[NIC] " << std::to_string(N_BUFFERS) << " buffers created\n";
+    db<NIC>(TRC) << "[NIC] [constructor] number of buffers created: " << std::to_string(N_BUFFERS) << "\n";
     
+    db<NIC>(TRC) << "[NIC] [constructor] initializing free buffers counter semaphore\n";
     sem_init(&_buffer_sem, 0, N_BUFFERS);
+    
+    db<NIC>(TRC) << "[NIC] [constructor] initializing binary semaphore\n";
     sem_init(&_binary_sem, 0, 1);
-
-    // Setting default address
+    db<NIC>(TRC) << "[NIC] [constructor] binary semaphore initialized\n";
+    
     _address = Engine::mac_address();
+    db<NIC>(TRC) << "[NIC] [constructor] address set to: " << Ethernet::mac_to_string(_address) << "\n";
+    
+    // NOW it's safe to start the Engine - all NIC infrastructure is ready
+    db<NIC>(TRC) << "[NIC] [constructor] starting Engine after full NIC initialization\n";
+    db<NIC>(TRC) << "[NIC] [constructor] NIC fully initialized and Engine started\n";
 }
 
 template <typename Engine>
@@ -127,13 +136,13 @@ NIC<Engine>::~NIC() {
 
     sem_destroy(&_buffer_sem);
     sem_destroy(&_binary_sem);
-    db<NIC>(INF) << "[NIC] semaphores destroyed\n";
+    db<NIC>(INF) << "[NIC] [destructor] semaphores destroyed\n";
     // Engine stops itself on its own
 }
 
 template <typename Engine>
 void NIC<Engine>::stop() {
-    db<NIC>(TRC) << "NIC<Engine>::stop() called!\n";
+    db<NIC>(TRC) << "[NIC] [stop()] called!\n";
     _running.store(false, std::memory_order_release);
     int sem_value;
     sem_getvalue(&_buffer_sem, &sem_value);
@@ -222,11 +231,11 @@ int NIC<Engine>::receive(DataBuffer* buf, Address* src, Address* dst, void* data
 
 template <typename Engine>
 void NIC<Engine>::handle(Ethernet::Frame* frame, unsigned int size) {
-    db<NIC>(TRC) << "NIC::handle() called!\n";
+    db<NIC>(TRC) << "[NIC] [handle()] called!\n";
 
     // Filter check (optional, engine might do this): ignore packets sent by self
     if (frame->src == _address) {
-        db<NIC>(INF) << "[NIC] ignoring frame from self: {src=" << Ethernet::mac_to_string(frame->src) << "}\n";
+        db<NIC>(INF) << "[NIC] [handle()] ignoring frame from self: {src=" << Ethernet::mac_to_string(frame->src) << "}\n";
         return;
    }
 
@@ -237,55 +246,69 @@ void NIC<Engine>::handle(Ethernet::Frame* frame, unsigned int size) {
     // 2. Allocate buffer
     unsigned int packet_size = size - Ethernet::HEADER_SIZE;
     if (packet_size == 0) {
-        db<NIC>(INF) << "[NIC] dropping empty frame\n";
+        db<NIC>(INF) << "[NIC] [handle()] dropping empty frame\n";
         _statistics.rx_drops++;
         return;
     }
 
+    db<NIC>(TRC) << "[NIC] [handle()] allocating buffer\n";
     DataBuffer * buf = alloc(dst, proto, packet_size);
+    db<NIC>(TRC) << "[NIC] [handle()] buffer allocated\n";
 
     if (!buf) {
-        db<NIC>(ERR) << "[NIC] alloc called, but NIC is not running\n";
+        db<NIC>(ERR) << "[NIC] [handle()] alloc called, but NIC is not running\n";
         return;
     }
 
     // 3. Fill RX timestamp in the buffer
+    db<NIC>(TRC) << "[NIC] [handle()] filling RX timestamp in the buffer\n";
     auto& clock = Clock::getInstance();
+    db<NIC>(TRC) << "[NIC] [handle()] getting synchronized time from clock\n";
     bool is_sync;
+    db<NIC>(TRC) << "[NIC] [handle()] getting synchronized time from clock\n";
     buf->setRX(clock.getSynchronizedTime(&is_sync).time_since_epoch().count());
+    db<NIC>(TRC) << "[NIC] [handle()] RX timestamp filled in the buffer\n";
 
     // 4. Copy frame to buffer
+    db<NIC>(TRC) << "[NIC] [handle()] copying frame to buffer\n";
     buf->setData(frame, size);
+    db<NIC>(TRC) << "[NIC] [handle()] frame copied to buffer\n";
 
    if (!running()) {
-        db<NIC>(ERR) << "[NIC] trying to notify protocol when NIC is inactive\n";
+        db<NIC>(ERR) << "[NIC] [handle()] trying to notify protocol when NIC is inactive\n";
         return;
     }
 
     // 5. Notify Observers
     if (!notify(buf, proto)) {
-        db<NIC>(INF) << "[NIC] data received, but no one was notified (" << proto << ")\n";
+        db<NIC>(INF) << "[NIC] [handle()] data received, but no one was notified (" << proto << ")\n";
         free(buf); // if no one is listening, release buffer
     }
 }
 
 template <typename Engine>
 typename NIC<Engine>::DataBuffer* NIC<Engine>::alloc(Address dst, Protocol_Number prot, unsigned int size) {
-    db<NIC>(TRC) << "NIC<Engine>::alloc() called!\n";
+    db<NIC>(TRC) << "[NIC] [alloc()] called!\n";
 
     if (!running()) {
-        db<NIC>(ERR) << "[NIC] alloc() called when NIC is inactive\n";
+        db<NIC>(ERR) << "[NIC] [alloc()] called when NIC is inactive\n";
         return nullptr;
     }
     
     // Acquire free buffers counter semaphore
+    db<NIC>(TRC) << "[NIC] [alloc()] acquiring free buffers counter semaphore\n";
     sem_wait(&_buffer_sem);
+    db<NIC>(TRC) << "[NIC] [alloc()] free buffers counter semaphore acquired\n";
     
     // Remove first buffer of the free buffers queue
+    db<NIC>(TRC) << "[NIC] [alloc()] acquiring binary semaphore\n";
     sem_wait(&_binary_sem);
+    db<NIC>(TRC) << "[NIC] [alloc()] binary semaphore acquired\n";
     DataBuffer* buf = _free_buffers.front();
     _free_buffers.pop();
+    db<NIC>(TRC) << "[NIC] [alloc()] buffer removed from free buffers queue\n";
     sem_post(&_binary_sem);
+    db<NIC>(TRC) << "[NIC] [alloc()] binary semaphore released\n";
 
     // Set Frame
     Ethernet::Frame frame;
@@ -297,7 +320,7 @@ typename NIC<Engine>::DataBuffer* NIC<Engine>::alloc(Address dst, Protocol_Numbe
     // Set buffer data
     buf->setData(&frame, frame_size);
 
-    db<NIC>(INF) << "[NIC] buffer allocated for frame: {src = " << Ethernet::mac_to_string(frame.src) << ", dst = " << Ethernet::mac_to_string(frame.dst) << ", prot = " << std::to_string(frame.prot) << ", size = " << buf->size() << "}\n";
+    db<NIC>(INF) << "[NIC] [alloc()] buffer allocated for frame: {src = " << Ethernet::mac_to_string(frame.src) << ", dst = " << Ethernet::mac_to_string(frame.dst) << ", prot = " << std::to_string(frame.prot) << ", size = " << buf->size() << "}\n";
 
     return buf;
 }
--- include/api/network/socketEngine.h
+++ include/api/network/socketEngine.h
@@ -239,49 +239,55 @@ int SocketEngine::send(Ethernet::Frame* frame, unsigned int size) {
 }
 
 void* SocketEngine::run(void* arg)  {
-    db<SocketEngine>(TRC) << "SocketEngine::run() called!\n";
+    db<SocketEngine>(TRC) << "[SocketEngine] [run()] called!\n";
 
     SocketEngine* engine = static_cast<SocketEngine*>(arg);
 
     struct epoll_event events[1024];
 
     while (engine->running()) {
+        db<SocketEngine>(TRC) << "[SocketEngine] [run()] epoll_wait() called\n";
         int n = epoll_wait(engine->_ep_fd, events, 1024, -1);
 
-        db<SocketEngine>(TRC) << "[SocketEngine] epoll event detected\n";
+        db<SocketEngine>(TRC) << "[SocketEngine] [run()] epoll event detected\n";
         
         if (n < 0) {
+            db<SocketEngine>(TRC) << "[SocketEngine] [run()] epoll_wait() returned error: " << errno << "\n";
             if (errno == EINTR) continue;
+            db<SocketEngine>(TRC) << "[SocketEngine] [run()] epoll_wait() returned error: " << errno << "\n";
             perror("epoll_wait");
             break;
         }
 
         // Iterates over all events detected by epoll
         for (int i = 0; i < n; ++i) {
+            db<SocketEngine>(TRC) << "[SocketEngine] [run()] epoll event " << i << " detected\n";
             int fd = events[i].data.fd;
 
             if (fd == engine->_sock_fd) {
-                db<SocketEngine>(INF) << "[SocketEngine] epoll socket event detected\n";
+                db<SocketEngine>(INF) << "[SocketEngine] [run()] epoll socket event detected\n";
                 engine->receive();
+                db<SocketEngine>(TRC) << "[SocketEngine] [run()] receive() called\n";
             } else if (fd == engine->_stop_ev) {
-                db<SocketEngine>(INF) << "[SocketEngine] epoll stop event detected\n";
+                db<SocketEngine>(INF) << "[SocketEngine] [run()] epoll stop event detected\n";
                 uint64_t u;
                 read(engine->_stop_ev, &u, sizeof(u)); // clears eventfd
+                db<SocketEngine>(TRC) << "[SocketEngine] [run()] stop event cleared\n";
                 break; // Next loop, thread will finish
             }
         }
     }
 
-    db<SocketEngine>(INF) << "[SocketEngine] receive thread terminated!\n";
+    db<SocketEngine>(INF) << "[SocketEngine] [run()] receive thread terminated!\n";
     return nullptr;
 };
 
 void SocketEngine::receive() {
-    db<SocketEngine>(TRC) << "SocketEngine::receive() called!\n";
+    db<SocketEngine>(TRC) << "[SocketEngine] [receive()] called!\n";
 
     // Checks weather engine is still active
     if (!running()) {
-        db<SocketEngine>(ERR) << "[SocketEngine] receive() called when engine is inactive\n";
+        db<SocketEngine>(ERR) << "[SocketEngine] [receive()] called when engine is inactive\n";
         return;
     }
 
@@ -293,7 +299,7 @@ void SocketEngine::receive() {
     
     // Checks weather receive was sucessful
     if (bytes_received < 0) {
-        db<SocketEngine>(INF) << "[SocketEngine] no data received\n";
+        db<SocketEngine>(INF) << "[SocketEngine] [receive()] no data received\n";
         if (errno != EAGAIN && errno != EWOULDBLOCK) {
             perror("recvfrom");
         }
@@ -302,13 +308,13 @@ void SocketEngine::receive() {
 
     // Checks for valid Ethernet frame size (at least header size)
     if (static_cast<unsigned int>(bytes_received) < Ethernet::HEADER_SIZE) {
-        db<SocketEngine>(ERR) << "[SocketExternalEngine] Received undersized frame (" << bytes_received << " bytes)\n";
+        db<SocketEngine>(ERR) << "[SocketEngine] [receive()] Received undersized frame (" << bytes_received << " bytes)\n";
         return;
     }
     
     // Convert protocol from network to host byte order
     frame.prot = ntohs(frame.prot);
-    db<SocketEngine>(INF) << "[SocketEngine] received frame: {src = " << Ethernet::mac_to_string(frame.src) << ", dst = " << Ethernet::mac_to_string(frame.dst) << ", prot = " << frame.prot << ", size = " << bytes_received << "}\n";
+    db<SocketEngine>(INF) << "[SocketEngine] [receive()] received frame: {src = " << Ethernet::mac_to_string(frame.src) << ", dst = " << Ethernet::mac_to_string(frame.dst) << ", prot = " << frame.prot << ", size = " << bytes_received << "}\n";
 
     this->handle(&frame, static_cast<unsigned int>(bytes_received));
 }
